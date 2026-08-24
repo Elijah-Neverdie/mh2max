1aed6de168ebb289ff414363bf8dbc51930600f0
Elijah-Neverdie <344383949@qq.com>
2026-08-24 11:52:26 +0800

    Add plug-ins/mh2max.py

---
+++ plug-ins/mh2max.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Maya plugin stub. UI is installed later from userSetup so splash is not blocked."""
+from __future__ import print_function
+
+import sys
+
+import maya.OpenMayaMPx as omp
+
+PLUGIN_VENDOR = "mh2max"
+PLUGIN_VERSION = "1.3.2"
+
+
+def initializePlugin(mobject):
+    omp.MFnPlugin(mobject, PLUGIN_VENDOR, PLUGIN_VERSION, "Any")
+    sys.stdout.write("[mh2max] plugin registered (menu comes from userSetup)\n")
+
+
+def uninitializePlugin(mobject):
+    omp.MFnPlugin(mobject)
+    try:
+        import mh2max.menu as menu
+
+        menu.uninstall()
+    except Exception:
+        pass
+    sys.stdout.write("[mh2max] plugin unloaded\n")
